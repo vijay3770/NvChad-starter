71d0977bfe5fcab16cf2e9d522e12bbd779c1aab
Vijay Adusumilli <vijay3770@gmail.com>
2025-07-08 22:05:17 -0500

    Add keybind for git line reset and preview

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -1,3 +1,5 @@
 local map = vim.keymap.set
 
--- add custom mappings here
+-- Map git stuff
+map('n', '<leader>grl', ':Gitsigns reset_hunk<CR>', {noremap = true, silent = true, desc = 'Undo line with Git'})
+map('n', '<leader>gp', ':Gitsigns preview_hunk<CR>', {noremap = true, silent = true, desc = 'Git preview'})
