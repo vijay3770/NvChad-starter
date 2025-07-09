415932ae1ffd50bc714236a9be4f2fa5b123ef69
Vijay Adusumilli <vijay3770@gmail.com>
2025-07-08 22:36:26 -0500

    Add LSP support with manson for c++, py, rust

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,4 +1,5 @@
 local plugins = {
+  -- auto sessions reopens the buffers previously kept open
   {
     'rmagatti/auto-session',
     lazy = false,
@@ -11,6 +12,49 @@ local plugins = {
       -- log_level = 'debug',
     }
   },
+  -- add lsp support for c++, python, rust
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      {
+        "williamboman/mason.nvim",
+        build = ":MasonUpdate",
+        config = true,
+      },
+      {
+        "williamboman/mason-lspconfig.nvim",
+        version = "v1.23.0",
+      },
+      {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+          "hrsh7th/cmp-nvim-lsp",
+        },
+      },
+    },
+    config = function()
+      local lspconfig = require("lspconfig")
+      local mason_lspconfig = require("mason-lspconfig")
+
+      -- Set up Mason LSP manager
+      mason_lspconfig.setup({
+        ensure_installed = { "clangd", "pyright", "rust_analyzer" },
+      })
+
+      -- nvim-cmp capabilities for LSP
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      -- Apply settings for each LSP server
+      mason_lspconfig.setup_handlers({
+        function(server_name)
+          lspconfig[server_name].setup({
+            capabilities = capabilities,
+          })
+        end,
+      })
+    end,
+  }
 }
 
 return plugins
